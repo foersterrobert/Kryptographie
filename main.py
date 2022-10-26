af1bef93083c4624ca367b5c6f4ec79e1f811383
robert foerster <foersteroberthh@gmail.com>
2022-10-26 19:13:14 +0200

    letter distribution + mathe

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import math
 import numpy as np
 
 st.set_page_config(layout="wide")
-st.title("Caeser-Verschlüsselung")
+st.title("Caesar-Verschlüsselung")
 st.subheader("Encode")
 encode_text_input = st.text_input("Enter text to encode:")
 encode_text_cleaned = encode_text_input.replace(
@@ -44,7 +44,7 @@ language = st.selectbox(
 
 d = enchant.Dict(language)
 
-letter_distribution_de = {
+letter_distribution = {
     'a': 6.51,
     'b': 1.89,
     'c': 3.06,
@@ -71,35 +71,6 @@ letter_distribution_de = {
     'x': 0.03,
     'y': 0.04,
     'z': 1.13
-}
-
-letter_distribution_en = {
-    'a': 8.2,
-    'b': 1.5,
-    'c': 2.8,
-    'd': 4.3,
-    'e': 13,
-    'f': 2.2,
-    'g': 2,
-    'h': 6.1,
-    'i': 7,
-    'j': 0.15,
-    'k': 0.77,
-    'l': 4,
-    'm': 2.4,
-    'n': 6.7,
-    'o': 7.5,
-    'p': 1.9,
-    'q': 0.095,
-    'r': 6,
-    's': 6.3,
-    't': 9.1,
-    'u': 2.8,
-    'v': 0.98,
-    'w': 2.4,
-    'x': 0.15,
-    'y': 2,
-    'z': 0.074
 }
 
 if decode_button:
@@ -147,16 +118,14 @@ if dictsolve:
             key=lambda x: solveMap[x]['idx'],
             reverse=True
         )
-        for key in solveMapKeys:
+        for key in solveMapKeys[:5]:
             st.text(
                 f'{solveMap[key]["str"]} at index {key}')
     else:
         st.text("No solution found")
 
 if charsolve:
-    # solveMap = {
-    #     k:0 for k in ascii_lowercase
-    # }
+    solveMap = {}
     for i in range(len(ascii_lowercase) - 1):
         solve_text = ''
         for char in decode_text_input:
@@ -169,11 +138,23 @@ if charsolve:
                 solve_text += char
         
         solve_text_ascii = solve_text.encode("ascii", "ignore").decode().replace(' ', '')
-        solve_text_distribution = {}
+        solve_text_distribution = {k: 0 for k in ascii_lowercase}
         for l in solve_text_ascii:
-            if l in solve_text_distribution.keys():
-                solve_text_distribution[l] += 1
+            if l.lower() in solve_text_distribution.keys():
+                solve_text_distribution[l.lower()] += 1
+        solve_text_distribution = {k: v / len(solve_text_ascii) * 100 for k, v in solve_text_distribution.items()}
+        solveMap[i] = {'str': solve_text, 'idx': sum([abs(letter_distribution[k] - solve_text_distribution[k]) for k in letter_distribution.keys()])}
     
+    solveMapKeys = sorted(
+        solveMap.keys(),
+        key=lambda x: solveMap[x]['idx'],
+        reverse=False
+    )
+    for key in solveMapKeys[:5]:
+        st.text(
+            f'{solveMap[key]["str"]} at index {key}')
+
+
 st.title("Vigenere Cipher")
 st.subheader("Encode")
 encode_text_input_vi = st.text_input("Enter text to encode:", key="encode_text_input_vi")
@@ -314,7 +295,7 @@ d: {st.session_state['rsa_d']}
 """)
 
 st.subheader("Encode")
-encode_message_rsa = st.text_input("Enter message to encode:", key="encode_message_rsa", value="hallo")
+encode_message_rsa = st.text_input("Enter message to encode:", key="encode_message_rsa")
 encode_button_rsa = st.button("Encode", key="encode_button_rsa")
 
 if encode_button_rsa:
@@ -331,7 +312,8 @@ if decode_button_rsa:
     decode_message_ascii_rsa = [pow(c, st.session_state['rsa_d'], st.session_state['rsa_n']) for c in decode_message_ascii_rsa]
     st.success("".join([chr(c) for c in decode_message_ascii_rsa]))
 
-with st.expander("Aufgaben Modulo"):
+st.title("Mathe Aufgaben")
+with st.expander("1. Modulo"):
     st.write(r"""
 #### Aufgabe 1:
 a) 27 mod 4 = 3 | print(27 % 4) $\rightarrow$ 4 * 6 + 3 = 27\
@@ -343,7 +325,7 @@ f) 37 mod 10 = 7 | print(37 % 10) $\rightarrow$ 10 * 3 + 7 = 37\
 g) 100037 mod 10 = 7 | print(100037 % 10) $\rightarrow$ 10 * 10003 + 7 = 100037\
 h) 107 mod 4 = 3 | print(107 % 4) $\rightarrow$ 4 * 26 + 3 = 107\
 i) 1 mod 2 = 1 | print(1 % 2) $\rightarrow$ 2 * 0 + 1 = 1\
-j) 3 mod 2 = 1 | print(3 % 2) $\rightarrow$ 2 * 1 + 1 = 3\
+j) 3 mod 2 = 1 | print(3 % 2) $\rightarrow$ 2 * 1 + 1 = 3
 
 #### Aufgabe 2:
 a) sei k eine gerade Zahl. Berechne k mod 2\
@@ -385,7 +367,7 @@ print((25 ** 12) % 4, (25 % 4) ** 12 % 4) $\rightarrow$ 1, 1
 #### Aufgabe 4:
 a) Es sei n irgendeine natürliche Zahl, die mit den Ziffern ...34 endet. Berechne n mod 4\
 34 mod 4 = 2 | print(34 % 4) $\rightarrow$ 4 * 8 + 2 = 34\
-12134 mod 4 = 2 | print(12134 % 4) $\rightarrow$ 4 * 3033 + 2 = 12134\
+12134 mod 4 = 2 | print(12134 % 4) $\rightarrow$ 4 * 3033 + 2 = 12134
 
 b) Wie kann man leicht überprüfen, ob eine Zahl durch 4 teilbar ist?\
 Die Zahl n ist durch 4 teilbar, wenn die letzte Ziffer 0, 4, 8 oder 2 ist.\
@@ -399,7 +381,7 @@ Der Zahnarzttermin ist am Freitag um 12 Uhr und der Computerkurs am Samstag um 8
         """
     )
 
-with st.expander("Aufgaben Restklassen"):
+with st.expander("2. Restklassen"):
     st.write(r"""
 Restklassen
 [a]$_{m}$ = {b $\in$ $\mathbf{Z}$ | $\exists$K $\in$ $\mathbf{Z}$ : b = k * m + a} = {b | b $\equiv$ a mod m}\
@@ -533,37 +515,89 @@ Multiplikationstabelle:
         )
     )
 
-with st.expander("Modulares Potenzieren"):
+with st.expander("3. modulares Potenzieren"):
     st.write(r"""
 #### Regeln
-(a·b) mod m = (a mod m·b mod m) mod m\
-(a**b) mod m = (a mod m)**b mod m
+(a $\cdot$ b) mod m = (a mod m $\cdot$ b mod m) mod m\
+(a$^{b}$) mod m = (a mod m)$^{b}$ mod m
 
 #### Aufgabe 1:
-Für 7 ** 4 mod 12 gilt die Form (a**b) mod m\
-Diese wurde in (a ** (b/2) * a ** (b/2)) mod m umgewandelt\
-Nach der ersten Regel lässt sich dies nun in (a ** (b/2) mod m * a ** (b/2) mod m) mod m umwandeln\
-Somit (49 % 12 * 49 % 12) % 12 = (1 * 1) % 12 = 1
+Für 7$^{4}$ mod 12 gilt die Form (a$^{b}$) mod m\
+Diese wurde in (a$^{\frac{b}{2}}$ $\cdot$ a$^{\frac{b}{2}}$) mod m umgewandelt.\
+Nach der ersten Regel lässt sich dies nun in (a$^{\frac{b}{2}}$ mod m $\cdot$ a$^{\frac{b}{2}}$ mod m) mod m umwandeln\
+Somit: (49 mod 12 $\cdot$ 49 mod 12) mod 12 = (1 $\cdot$ 1) % 12 = 1
 
-Für 82 ** 17 mod 20 gilt die Form (a**b) mod m\
-Im nächsten Schritt wurde nach der zweiten oberen Regel das a=82 durch a=2 gekürzt, da sie sich in der selben Restklasse befinden. | 82 % 20 = 2 $\rightarrow%$ 20 * 4 + 2 | 2 % 20 = 2\
+Für 82$^{17}$ mod 20 gilt die Form (a$^{b}$) mod m\
+Im nächsten Schritt wurde nach der zweiten oberen Regel das a=82 durch a=2 gekürzt, da sie sich in der selben Restklasse befinden. | 82 mod 20 = 2 $\rightarrow%$ 20 $\cdot$ 4 + 2 | 2 mod 20 = 2\
 Nun wird der Term wieder in ein Produkt aufgeteilt, wodurch sich die erste obere Regel anwenden lässt.
-Nun kann auch 16 durch -4 ersetzt werden, da sie sich in der selben Restklasse befinden. | 16 % 20 = 16 $\rightarrow%$ 20 * 0 + 16 | -4 % 20 = 16\
-Somit (-4)**2 * 2 % 20 = (16 * 2) % 20 = 32 % 20 = 12
+Jetzt kann auch 16 durch -4 ersetzt werden, da sie sich in der selben Restklasse befinden. | 16 mod 20 = 16 $\rightarrow%$ 20 $\cdot$ 0 + 16 | -4 mod 20 = 16\
+Somit: (-4)$^{2}$ $\cdot$ 2 mod 20 = (16 $\cdot$ 2) mod 20 = 32 mod 20 = 12
 
 #### Aufgabe 2:
-a) 8 ** 9 mod 7 = 1 ** 9 mod 7 = 1\
-b) 6 ** 9 mod 7 = (-1) ** 9 mod 7 = 6\
-c) 54 ** 16 mod 55 = (-1) ** 16 mod 55 = 1\
+a) 8 ** 9 mod 7 = 1 ** 9 mod 7 = 1
+
+b) 6 ** 9 mod 7 = (-1) ** 9 mod 7 = 6
+
+c) 54 ** 16 mod 55 = (-1) ** 16 mod 55 = 1
+
 d) 3 ** 333 mod 26 = (3 ** 3) ** 100 mod 26\
     = (3 ** 3 % 26) ** 100 mod 26\
-    = 1 ** 100 mod 26\
-    = 1\
+    = 1 ** 100 mod 26 = 1
+
 e) 2 ** 268 mod 17 = (2 ** 4) ** 67 mod 17\
     = (2 ** 4 % 17) ** 67 mod 17\
     = (-1) ** 67 mod 17\
+    = (-1) % 17 = 16
+
 f) 2 ** 269 mod 17 = (2 ** 268 * 2) mod 17\
     = (2 ** 268 mod 17 * 2 mod 17) mod 17\
-    = 1 * 2 mod 17\
-    = 2
+    = (16 * 2) mod 17 = 15
+
+g) 2 ** 270 mod 19 = (2 ** 9) ** 30 mod 19\
+    = (2 ** 9 % 19) ** 30 mod 19\
+    = (-1) ** 30 mod 19 = 1
+
+h) 2 ** 271 mod 19 = (2 ** 270 * 2) mod 19\
+    = (2 ** 270 mod 19 * 2 mod 19) mod 19\
+    = (1 * 2) mod 19 = 2
+
+i) 3 ** 333 mod 15 = (3 * 3 ** 332) mod 15\
+    = (3 * (3 ** 4) ** 83) mod 15\
+    = (3 * 81 ** 83) mod 15\
+    = (3 * 6 ** 83) mod 15\
+    = (3 mod 15 * 6 ** 83 mod 15) mod 15\
+    = 3 * 6 mod 15 = 3 | Weil 6 ** k mod 15 = 6
+    """)
+
+with st.expander("4. sym-asym Verschlüsselung"):
+    st.write(r"""
+#### Aufgabe 1: Was ist der Nachteil an der symmetrischen Verschlüsselung?
+Bei der symmetrischen Verschlüsselung wird der selbe Schlüssel zum Verschlüsseln und Entschlüsseln verwendet.
+Dieser Schlüssel muss also geheim gehalten werden, wird er jedoch beim Verschlüsseln abgefangen, so kann ein Dritter ihn missbrauchen um wiederum Nachrichten zu entschlüsseln.
+
+#### Aufgabe 2: Fasse die Schritte der asymmetrischen Verschlüsselung in Worte!
+1.
+2.
+3.
+4.
+    """)
+
+with st.expander("5. Einwegfunktionen"):
+    st.text(r"""
+
+    """)
+
+with st.expander("6. RSA auf einen Blick"):
+    st.text(r"""
+
+    """)
+
+with st.expander("7. euklidischer Algorithmus"):
+    st.text(r"""
+
+    """)
+
+with st.expander("8. multiplikatives Inverses"):
+    st.text(r"""
+
     """)
